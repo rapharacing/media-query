30d19fc6d0cae9dc0ceb5850188060c4ae9d609d
jjkilpatrick <john.kilpatrick@rapha.cc>
2019-08-09 16:00:26 +0100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js"
-  }
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                }
+            }
+        ]
+    }
 };
